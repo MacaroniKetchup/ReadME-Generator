484203534417bd4d2b0c90849c63abc69ac726f0
MacaroniKetchup <stylesthestyer@gmail.com>
2023-02-27 11:35:19 -0700

    wrote code to get application to run prompts, getting an error in saving and generating the file though...

--- index.js
+++ index.js
@@ -1,99 +1,105 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require ('util');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
+    // Asks for title name
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What do you want to name your project?',
+        default: 'Opsies woopsies, no project name',
+        // Validates to check if user has a valid input
+        validate: (value) => { if (value) {return true} else {return 'Guess you already forgot the name of your project? > ->'}},
+    },
+    // Asks for your given name
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+        // Validates to check if user has a valid input
+        validate: (value) => { if (value) {return true} else {return 'Must be getting old when you start forgetting your name? > ->'}},
+    },
+    // Asks for your username
     {
     type: 'input',
     name: 'username',
     message: 'What is your Github User name?',
     default: 'MacaroniKetchup',
-    validate: function (answer) {
-        if (answer.length < 1) {
-            return console.log('Did you forget what your username was?');
-        }
-        return true;
-    }
+    // Validates to check if user has a valid input
+    validate: (value) => { if (value) {return true} else {return 'Must be getting old when you start forgetting your username? > ->'}},
 },
-{
-    type: 'input',
-    name: 'repo-name',
-    message: 'What is the name of your repository?',
-    default: 'They forgot to name there project lol',
-    validate: function (answer) {
-        if (answer.length < 1) {
-            return console.log('Do you not know what to name your repo??');
-        }
-        return true;
-    }
-},
-{
-    type: 'input',
-    name: 'title',
-    message: 'What do you want to name your project?',
-    default: 'Opsies woopsies, no project name',
-    validate: function (answer) {
-        if (answer.length <1) {
-            return console.log('Remember to name your project like its your first born child!');
-        }
-        return true;
-    }
-},
+// Asks for a breif description of the project
 {
     type: 'input',
     name: 'description',
     message: 'Tell me about your project!',
     default: 'They didnt want to share their secrets...  ( TAT)',
-    validate: function (answer) {
-        if (answer.length <1) {
-            return console.log('Some of us would like to know about your project pwease... ( u_u)');
-        }
-        return true;
-    }
+     // Validates to check if user has a valid input
+    validate: (value) => { if (value) {return true} else {return 'Did you not have anything to say about your project? > ->'}},
 },
+// Asks if there are installs required for the project
 {
     type: 'input',
     name: 'installations',
     message: 'Is there any installs that will be needed for this project?',
 },
+// Asks for any steps to follow in running your applications
 {
     type: 'input',
     name: 'usage',
     message: 'Please explain steps on how to use your project!',
 },
+// Asks for a list of any contributors in the project
 {
     type:'input',
     name:'contibutions',
     message:'Please list any instructions on how contributions can be made to this project!',
 },
+// Asks for any tests written for this project and how to access them
 {
     type:'input',
     name:'tests',
-    message:'If possible, please give any test written for this project and how access/use them!',
+    message:'If possible, please give any tests written for this project and how access/use them!',
 },
+// A list of licesne options to select from
 {
     type:'list',
     name:'license',
     choices:['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
     message:'Please choose a license for this project',
+},
+// Asks for an email for further contact
+{
+    type: 'input',
+    name: 'email',
+    message: 'Input your email so viewers may contact you for questions!',
 }
 ];
 
 // TODO: Create a function to write README file
+// Function to write the README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-           return console.log(err);
-        } 
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data, (err) => {
+        if(err){
+            console.log(err)
+        }
         console.log('Your README file has been successfully generated!! (-^ U ^-);')
-    })
- }
+    });
+}
+
+
 
 // TODO: Create a function to initialize app
+// Function that initializes the up
 function init() {
-    
+    inquirer.prompt(questions).then((responses) => {
+        console.log('Creating a super awesome and totally professional README.md file for you! (-^ U ^-); ');
+        writeToFile('./dist/README.md', generateMarkdown({...responses}));
+    });
  }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,65 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+// Function that displays badge for which license is being used
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github license](https://img.sheilds.io/badge/license-${license}-yellowgreen.svg)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
+// Function that displays the link to the licesne
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'none') {
+    return `\n* [license](#license)\n`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
+// Function that displays license section in the README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'none') {
+    return `## License
+
+      licensed under the ${license} license.`
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
+// Template to generate markdown README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+by ${data.name},${data.username}
+## Table of Contents
+- [Description](#description)
+- [Installations](#installations)
+- [Usage](#usage)
+- [Contact-Info](#contact)
+- [Contributions](#contributions)
+- [Testing](#testing)
+- ${renderLicenseLink(data.license)}
+## Description
+${data.description}
+## Installations
+${data.installations}
+## Usage
+${data.usage}
+## Contibutors
+${data.contributions}
+## Contact-Info
+- Name: ${data.name}
+- Email: ${data.email}
+- Github: ${data.username}(https://github.com/${data.username}/)
+## Testing
+================================================================
+${data.test}
+================================================================
+${renderLicenseSection(data.licesne)}
 `;
 }
 
